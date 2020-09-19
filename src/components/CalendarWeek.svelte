3b9ec18b82eb3a7de0a9cf6e9480e0350417d78b
Kenneth Gitere <gitere81@gmail.com>
2020-09-19 08:14:06 +0300

    Add mobile view for week view. This will combine week and day view
    Minor refactor in CalendarWeek to use functions instead of inlining booleans
    for readability

--- src/components/CalendarWeek.svelte
+++ src/components/CalendarWeek.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import { today, selectedDate } from "../stores";
   import type { Dayjs } from "dayjs";
-  import type { formats } from "dayjs/locale/*";
+
+  const isToday = (date: Dayjs) => date.format("YYYY-MM-DD") == $today;
+  const isSelected = (date: Dayjs) =>
+    date.format("YYYY-MM-DD") == $selectedDate.format("YYYY-MM-DD");
 
   let dates: Dayjs[] = Array(7);
   $: {
@@ -17,14 +20,20 @@
 <style>
   .parent {
     display: grid;
-    grid-template-columns: 60px repeat(7, 1fr);
+    grid-template-columns: 2.5rem repeat(7, 1fr);
+  }
+  @screen md {
+    .parent {
+      display: grid;
+      grid-template-columns: 3rem repeat(7, 1fr);
+    }
   }
 </style>
 
 <div>
-  <div class="flex h-24">
+  <div class="flex h-16 md:h-24">
     <!-- Refactor this to be usable from CalendarWeekdays -->
-    <div style="width: 60px" class="flex justify-center items-center">
+    <div class="w-12 hidden md:flex justify-center items-center">
       <svg
         class="w-6 h-6 text-gray-500"
         fill="none"
@@ -39,20 +48,20 @@
     {#each dates as date (date.format('YYYY-MM-DD'))}
       <div
         on:click={() => ($selectedDate = date)}
-        class="flex-1 flex flex-col justify-center bg-gray-400 h-full
-          cursor-pointer"
-        class:text-blue-600={date.format('YYYY-MM-DD') == $selectedDate.format('YYYY-MM-DD')}
-        class:text-bright-yellow={date.format('YYYY-MM-DD') == $today}>
-        <p class="text-center text-2xl font-bold">{date.format('DD')}</p>
-        <p class="text-center uppercase">{date.format('dddd')}</p>
+        class="flex-1 flex flex-col justify-center h-full cursor-pointer"
+        class:text-blue-600={isSelected(date)}
+        class:text-bright-yellow={isToday(date)}>
+        <p class="text-center md:text-2xl font-bold">{date.format('DD')}</p>
+        <p class="text-center md:uppercase">{date.format('dddd')}</p>
       </div>
     {/each}
   </div>
   <div class="parent grid h-102 border-gray-400 overflow-y-scroll">
+    <!-- Extract this into a timeline component -->
     {#each { length: 24 * 8 } as _, i}
       {#if i % 8 == 0}
-        <div class="border-gray-400 h-20">
-          <p class="sticky top-0 p-1 text-gray-600">
+        <div class="h-20 border-b-2 border-gray-400 md:border-none">
+          <p class="sticky top-0 text-gray-600 text-xs md:pt-1 md:text-base">
             {Math.ceil(i / 8)
               .toString()
               .padStart(2, '0')}:00
